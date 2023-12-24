337a41e1d1cc3ea5a610da91dd9936904d7259c4
jpradas1 <jpradas@unal.edu.co>
2023-12-24 14:59:31 +0100

    Clousers and Decorators challenges

--- Python/15_Closures_and_Decorators/098_Decorators_2-Name_Directory.py
+++ Python/15_Closures_and_Decorators/098_Decorators_2-Name_Directory.py
@@ -1,3 +1,20 @@
+'''
+Let's use decorators to build a name directory! You are given some 
+information about `N` people. Each person has a first name, last name, 
+age and sex. Print their names in a specific format sorted by their age in 
+ascending order i.e. the youngest person's name should be printed first. 
+For two people of the same age, print them in the order of their input.
+
+For Henry Davids, the output should be:
+
+>>> Mr. Henry Davids
+
+For Mary George, the output should be:
+
+>>> Ms. Mary George
+'''
+
+
 import operator
 
 def person_lister(f):
